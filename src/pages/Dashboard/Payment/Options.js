ab77bc74074b0bc5e94544ce8ef91ee93e7c9994
gacdutra <ga.acdutra@gmail.com>
2022-12-19 11:56:31 -0300

    feat: added GET /ticket/types

--- src/pages/Dashboard/Payment/Options.js
+++ src/pages/Dashboard/Payment/Options.js
@@ -1,22 +1,25 @@
 import { useState } from 'react';
 import styled from 'styled-components';
+import useTicketType from '../../../hooks/api/useTicket';
 import OptionBox from './OptionBox';
 
 export default function Options() {
   const [optionSelected, setOptionSelected] = useState('');
+  const { ticketTypes } = useTicketType();
+
   return (
     <OptionsWrapper>
       <OptionBox
         optionSelected={optionSelected}
         setOptionSelected={setOptionSelected}
-        description={'Presencial'}
-        price={'250'}
+        description={ticketTypes && ticketTypes[1] ? ticketTypes.name : 'Presencial'}
+        price={ticketTypes && ticketTypes[1] ? ticketTypes.price : '250'}
       ></OptionBox>
       <OptionBox
         optionSelected={optionSelected}
         setOptionSelected={setOptionSelected}
-        description={'Online'}
-        price={'100'}
+        description={ticketTypes && ticketTypes[2] ? ticketTypes.name : 'Online'}
+        price={ticketTypes && ticketTypes[2] ? ticketTypes.price : '100'}
       ></OptionBox>
     </OptionsWrapper>
   );

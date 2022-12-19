c89588a7329e9920585a2352cd29eb8d88240330
anna-braga-jesus <annajesus.nave@gmail.com>
2022-12-19 14:27:46 -0300

    feat: add online choosed

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -7,6 +7,8 @@ export default function Payment() {
       <Title>Ingresso e pagamento</Title>
       <SubTitle>Primeiro, escolha sua modalidade de ingresso</SubTitle>
       <Options />
+      <SubTitle>Fechado! O total ficou em <b>R$100</b>. Agora é só confirmar.</SubTitle>
+      <Button>Reservar ingresso</Button>
     </>
   );
 }
@@ -20,6 +22,31 @@ const Title = styled.h1`
 const SubTitle = styled.h2`
   font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
   font-size: 20px;
-  margin-bottom: 20px;
+  margin: 20px 0;
   color: #8e8e8e;
+  b{
+    font-weight: 700;
+  }
 `;
+
+const Button = styled.div`
+height: 37px;
+width: 200px;
+border-radius: 4px;
+display: flex;
+align-items: center;
+justify-content: center;
+text-transform: uppercase;
+box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+transition: all 0.3s ease 0s;
+cursor: pointer;
+font-family: 'Roboto',sans-serif;
+background-color: lightgray;
+border: none;
+outline: none;
+:hover{
+  background-color: #FFEED2;
+  box-shadow: 0px 15px 20px lightgray;
+  color: #000;
+  transform: translateY(-7px);
+};`;

5bb88537bf3ca9f401b79d01d5de8cfd75c97e43
gacdutra <ga.acdutra@gmail.com>
2022-12-20 15:01:30 -0300

    feat: add ticket with hotel option; 2. feat: add reserve ticket button

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,14 +1,9 @@
-import { useContext } from 'react';
-import TicketContext from '../../../contexts/TicketContext';
 import Options from './Options';
 import SubTitle from './Subtitle';
 import Title from './Title';
-import useTicketType from '../../../hooks/api/useTicket';
 import ReserveButton from './ReserveButton';
 
 export default function Payment() {
-  const { ticketTypeSelected, includesHotel } = useContext(TicketContext);
-  const { ticketTypes } = useTicketType();
   return (
     <>
       <Title>Ingresso e pagamento</Title>

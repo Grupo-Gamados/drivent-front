fc63122b0ebeb00e31f8baf08803fc040d0577a3
gacdutra <ga.acdutra@gmail.com>
2022-12-22 11:18:35 -0300

    feat: add reserved ticket description

--- src/contexts/TicketContext.js
+++ src/contexts/TicketContext.js
@@ -7,8 +7,18 @@ export default TicketContext;
 export function TicketProvider({ children }) {
   const [ticketTypeSelected, setTicketTypeSelected] = useState('');
   const [includesHotel, setIncludesHotel] = useState('');
+  const [ticketReserved, setTicketReserved] = useState({});
   return (
-    <TicketContext.Provider value={{ ticketTypeSelected, setTicketTypeSelected, includesHotel, setIncludesHotel }}>
+    <TicketContext.Provider
+      value={{
+        ticketTypeSelected,
+        setTicketTypeSelected,
+        includesHotel,
+        setIncludesHotel,
+        ticketReserved,
+        setTicketReserved,
+      }}
+    >
       {children}
     </TicketContext.Provider>
   );
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -2,14 +2,36 @@ import Options from './Options';
 import SubTitle from './Subtitle';
 import Title from './Title';
 import ReserveButton from './ReserveButton';
+import ChosenTicket from './ChosenTicket';
+import useTicket from '../../../hooks/api/useTicket';
+import { useContext, useEffect, useState } from 'react';
+import TicketContext from '../../../contexts/TicketContext';
 
 export default function Payment() {
+  const { ticket } = useTicket();
+  const [userTicket, setUserTicket] = useState({});
+  const { setTicketReserved } = useContext(TicketContext);
+
+  useEffect(() => {
+    if (ticket) {
+      setUserTicket(ticket);
+      setTicketReserved(ticket);
+    }
+  }, [userTicket]);
+
   return (
     <>
       <Title>Ingresso e pagamento</Title>
-      <SubTitle>Primeiro, escolha sua modalidade de ingresso</SubTitle>
-      <Options />
-      <ReserveButton></ReserveButton>
+      {ticket ? (
+        <ChosenTicket />
+      ) : (
+        <>
+          {' '}
+          <SubTitle>Primeiro, escolha sua modalidade de ingresso</SubTitle>
+          <Options />
+          <ReserveButton></ReserveButton>{' '}
+        </>
+      )}
     </>
   );
 }

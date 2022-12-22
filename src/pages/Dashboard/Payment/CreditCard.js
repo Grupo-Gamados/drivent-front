96a95e4f5001f47b0b5722a52e10f6d3c76fab24
anna-braga-jesus <annajesus.nave@gmail.com>
2022-12-22 16:05:41 -0300

    feat: add credit card layout

--- src/pages/Dashboard/Payment/CreditCard.js
+++ src/pages/Dashboard/Payment/CreditCard.js
@@ -1,15 +1,26 @@
-import { useContext } from 'react';
 import styled from 'styled-components';
-import TicketContext from '../../../contexts/TicketContext';
-import useTicketType from '../../../hooks/api/useTicket';
-import OptionBox from './OptionBox';
-import SubTitle from './Subtitle';
+import card from '../../../assets/images/card.png';
+
 export default function CreditCard() {
   return (
     <>
       <CreditCardWrapper>
-        <p>Teste</p>
+        <CardBox>
+          <MyCard>
+            <img src={card} alt='ta'></img>
+          </MyCard>
+          <MyCard>
+            <CardNumber placeholder='Card Number'></CardNumber>
+            <ExampleCard>E.g.:49..., 51..., 36..., 37...</ExampleCard>
+            <Name placeholder='Name'></Name>
+            <Valid>
+              <ValidThru placeholder='Valid Thru'></ValidThru>
+              <CVV placeholder='CVV'></CVV>
+            </Valid>
+          </MyCard>
+        </CardBox>
       </CreditCardWrapper>
+      <Button>Finalizar Pagamento</Button>
     </>
   );
 }
@@ -18,5 +29,97 @@ const CreditCardWrapper = styled.div`
   display: flex;
   > div {
     margin-right: 24px;
+  }
+`;
+
+const CardBox = styled.div`
+  height: 225px;
+  width: 600px;
+  border-radius: 0px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const MyCard = styled.div`
+height: 200px;
+width: 290px;
+border-radius: 20px;
+font-family: 'Roboto', sans-serif;
+font-size: 16px;
+img{
+  width: 100%;
+  object-fit: cover;
+}
+
+`;
+const CardNumber = styled.input`
+width: 100%;
+height: 40px;
+border-radius: 8px;
+margin: 5px 0;
+font-family: 'Roboto', sans-serif;
+font-size: 16px;
+border: 1px solid gray;
+`;
+const ExampleCard = styled.p`
+font-size: 16px;
+font-family: 'Roboto', sans-serif;
+font-size: 16px;
+color: gray;
+`;
+const Name = styled.input`
+width: 100%;
+height: 40px;
+border-radius: 8px;
+margin: 20px 0 2px 0;
+font-family: 'Roboto', sans-serif;
+font-size: 16px;
+border: 1px solid gray;
+`;
+const CVV = styled.input`
+width: 30%;
+height: 40px;
+border-radius: 8px;
+font-family: 'Roboto', sans-serif;
+font-size: 16px;
+border: 1px solid gray;
+`;
+const Valid = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+font-family: 'Roboto', sans-serif;
+font-size: 16px;
+`;
+const ValidThru = styled.input`
+width: 60%;
+height: 40px;
+border-radius: 8px;
+margin: 10px 0;
+font-family: 'Roboto', sans-serif;
+font-size: 16px;
+border: 1px solid gray;
+`;
+const Button = styled.div`
+  height: 37px;
+  width: 200px;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-transform: uppercase;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease 0s;
+  cursor: pointer;
+  font-family: 'Roboto', sans-serif;
+  background-color: lightgray;
+  border: none;
+  outline: none;
+  :hover {
+    background-color: #ffeed2;
+    box-shadow: 0px 15px 20px lightgray;
+    color: #000;
+    transform: translateY(-7px);
   }
 `;
--- src/pages/Dashboard/Payment/Subtitle.js
+++ src/pages/Dashboard/Payment/Subtitle.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const SubTitle = styled.h2`
   font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
   font-size: 20px;
-  margin: 20px 0;
+  margin: 10px 0;
   color: #8e8e8e;
   b {
     font-weight: 700;

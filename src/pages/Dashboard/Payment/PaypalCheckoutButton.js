ee385c028e39ae031e7d80ce7c45bf678f540091
anna-braga-jesus <annajesus.nave@gmail.com>
2023-01-06 15:38:22 -0300

    feat: add something new

--- src/pages/Dashboard/Payment/PaypalCheckoutButton.js
+++ src/pages/Dashboard/Payment/PaypalCheckoutButton.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 import { PayPalButtons } from '@paypal/react-paypal-js';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 function PaypalCheckoutButton() {
   return (
     <PayPalScriptProvider>
-      <PayPalButtons />
+      <BoxPayPal>
+        <PayPalButtons />
+      </BoxPayPal>
     </PayPalScriptProvider>
   );
 }
+
+const BoxPayPal = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+margin: auto;
+`;
 
 export default PaypalCheckoutButton;

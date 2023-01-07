e9ab83c9226b2ad84c874b86e256d22f6668d9c0
gacdutra <ga.acdutra@gmail.com>
2023-01-06 22:18:16 -0300

    feat: added booking api routes and hooks

---
+++ src/services/bookingApi.js
@@ -0,0 +1,43 @@
+import api from './api';
+
+export async function getBooking(token) {
+  const response = await api.get('/booking', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
+
+export async function getBookingByRoomId(token, roomId) {
+  const response = await api.get(`/booking/${roomId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
+
+export async function postBooking(token, roomId) {
+  const body = { roomId };
+  const response = await api.post('/booking', body, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
+
+export async function changeBooking(token, bookingId, roomId) {
+  const body = { roomId };
+  const response = await api.put(`/booking/${bookingId}`, body, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}

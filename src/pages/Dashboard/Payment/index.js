c702f52312c469790e574f27f5e0b660f31078b9
gacdutra <ga.acdutra@gmail.com>
2023-01-16 08:58:19 -0300

    refactor: delete useless states

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -4,21 +4,15 @@ import Title from './Title';
 import ReserveButton from './ReserveButton';
 import ChosenTicket from './ChosenTicket';
 import useTicket from '../../../hooks/api/useTicket';
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import TicketContext from '../../../contexts/TicketContext';
 import CreditCard from './CreditCard';
 
 export default function Payment() {
   const { ticket } = useTicket();
-  const [userTicket, setUserTicket] = useState({});
-  const { setTicketReserved, reloadTicket } = useContext(TicketContext);
+  const { reloadTicket } = useContext(TicketContext);
 
-  useEffect(() => {
-    if (ticket) {
-      setUserTicket(ticket);
-      setTicketReserved(ticket);
-    }
-  }, [userTicket, reloadTicket]);
+  useEffect(() => {}, [reloadTicket]);
 
   return (
     <>
